6e3ad6234239037e2578f15a260acf75afa11a33
Sergo Pasoevi <spasoevi@gmail.com>
2020-02-25 21:35:53 +0200

    Updates

---
+++ precache-manifest.d593e7ec947985c5e11ba276874b5b1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b1b0944cd25a7df8fde22273fa1bc66",
+    "url": "/currency-selection/index.html"
+  },
+  {
+    "revision": "c593148e5f7f03673db4",
+    "url": "/currency-selection/static/css/main.e2d3897d.chunk.css"
+  },
+  {
+    "revision": "03210fd653d9f77c1a40",
+    "url": "/currency-selection/static/js/2.61947680.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/currency-selection/static/js/2.61947680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c593148e5f7f03673db4",
+    "url": "/currency-selection/static/js/main.2ceab61e.chunk.js"
+  },
+  {
+    "revision": "193dca418e6398153bd7",
+    "url": "/currency-selection/static/js/runtime-main.8a00fea9.js"
+  }
+]);
